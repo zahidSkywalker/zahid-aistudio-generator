3d8fd0895f39ac1f71b559e636ff5a539abe1a15
Jahidul Islam <ionzahid0987@gmail.com>
2025-08-24 12:20:29 +0600

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,10 +1,10 @@
+// api/proxy.js
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
   try {
     const perchanceUrl = "https://perchance.org/zahid-aistudio";
 
-    // fetch the content
     const response = await fetch(perchanceUrl, {
       headers: {
         "User-Agent": req.headers["user-agent"] || "Mozilla/5.0"
@@ -13,10 +13,8 @@ export default async function handler(req, res) {
 
     const body = await response.text();
 
-    // send it back to the browser
     res.setHeader("Content-Type", "text/html; charset=utf-8");
     res.status(200).send(body);
-
   } catch (error) {
     res.status(500).send("Error fetching site: " + error.message);
   }
